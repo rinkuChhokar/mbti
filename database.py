ab596aaa7f06470adebcc418e797219fd3870973
rinkuChhokar <91409108+rinkuChhokar@users.noreply.github.com>
2022-08-25 23:11:14 +0530

    Update database.py

--- database.py
+++ database.py
@@ -9,9 +9,9 @@ import streamlit as st
 # DETA_KEY2 = st.secrets["dkey2"]
 
 
-deta = Deta(st.secrets["dkey"])
+deta = Deta(st.secrets.dkey)
 
-deta1 = Deta(st.secrets["dkey2"])
+deta1 = Deta(st.secrets.dkey2)
 
 db = deta.Base("info_db")
 
